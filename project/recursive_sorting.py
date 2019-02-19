cf605bf4bf939289814ccc59f5c19aee38991676
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-19 12:41:03 -0800

    implements quick_sort

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -6,10 +6,10 @@ def merge(arrA, arrB):
     b = 0
     # since arrA and arrB already sorted, we only need to compare the first element of each when merging!
     for i in range(elements):
-        if a >= len(arrA):    # all elements in arrA have been merged
+        if a == len(arrA):    # all elements in arrA have been merged
             merged_arr[i] = arrB[b]
             b += 1
-        elif b >= len(arrB):  # all elements in arrB have been merged
+        elif b == len(arrB):  # all elements in arrB have been merged
             merged_arr[i] = arrA[a]
             a += 1
         elif arrA[a] < arrB[b]:  # next element in arrA smaller, so add to final array
@@ -45,13 +45,32 @@ def merge_sort_in_place(arr, l, r):
 
 
 # TO-DO: implement the Quick Sort function below USING RECURSION
-def quick_sort(arr, low, high):
+def partition(arr, low, high):
+    i = low - 1
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 
+
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
     return arr
 
 
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+quick_sort(arr1, 0, len(arr1) - 1)
+
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
+
+
 def timsort(arr):
 
     return arr
